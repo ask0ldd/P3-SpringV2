f5288b83e8889d7634863a23f0be7cfae1ad8dbf
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-03-18 16:25:02 +0100

    message repository complete

---
+++ src/main/java/com/example/immo/repositories/MessageRepository.java
@@ -0,0 +1,10 @@
+package com.example.immo.repositories;
+
+import com.example.immo.models.Message;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends CrudRepository<Message, Long> {
+
+}
