c186bdbd1a491631ff1835f7e78b44797d996c71
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-03-19 15:31:40 +0100

    DefaultResponseDto impl

---
+++ src/main/java/com/example/immo/dto/responses/DefaultResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.immo.dto.responses;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class DefaultResponseDto {
+
+    private String message;
+
+    public DefaultResponseDto(String message) {
+        this.message = message;
+    }
+}
