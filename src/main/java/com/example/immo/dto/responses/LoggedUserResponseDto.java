81f53aa2b7e351386efa394808c23e3d30be933f
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-03-19 15:27:51 +0100

    TokenResponseDto impl

---
+++ src/main/java/com/example/immo/dto/responses/LoggedUserResponseDto.java
@@ -0,0 +1,27 @@
+package com.example.immo.dto.responses;
+
+import com.example.immo.models.User;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Data
+public class LoggedUserResponseDto {
+    private Long id;
+    private String name;
+    private String email;
+    private Date created_at;
+    private Date updated_at;
+
+    public LoggedUserResponseDto(User user) {
+        this.id = user.getUserId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.created_at = user.getCreation();
+        this.updated_at = user.getUpdate();
+    }
+}
