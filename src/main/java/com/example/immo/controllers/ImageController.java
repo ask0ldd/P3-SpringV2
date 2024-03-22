adeca34d64a82df87741cb5edbb99f36ced36136
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-03-22 13:19:25 +0100

    image controller working img serving method

--- src/main/java/com/example/immo/controllers/ImageController.java
+++ src/main/java/com/example/immo/controllers/ImageController.java
@@ -1,20 +1,17 @@
 package com.example.immo.controllers;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 import com.example.immo.services.FileService;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
-import org.springframework.util.StreamUtils;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 // 127.0.0.1:3001/img/rental/griff.jpg
+// 127.0.0.1:3001/img/getimage
+// 127.0.0.1:3001/img/images/griff.jpg
 @RestController
 @RequestMapping("img")
 @CrossOrigin(origins = "http://localhost:4200")
@@ -29,9 +26,20 @@ public class ImageController {
     @Value("${file.path}")
     private String filePath;
 
-    @GetMapping(value = "/rental/{imageName}", produces = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
-    public void getImage(HttpServletResponse response, @PathVariable String imageName) throws IOException {
-        InputStream stream = fileService.getResource(imageName);
-        StreamUtils.copy(stream, response.getOutputStream());
+    @GetMapping("/rental/{imageName}")
+    public ResponseEntity<Resource> serveImage(@PathVariable String imageName) {
+        try {
+            Resource resource = fileService.getImgResource(imageName);
+
+            if (resource.exists() || resource.isReadable()) {
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_TYPE, "image/jpeg") // !!! Change content type based on your image type
+                        .body(resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (IOException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
