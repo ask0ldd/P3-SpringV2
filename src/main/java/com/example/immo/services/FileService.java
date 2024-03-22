5c7fdd17319b6ec7bc1552a08c91cd720719dcb5
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-03-22 13:20:01 +0100

    file service : new get img resource method

--- src/main/java/com/example/immo/services/FileService.java
+++ src/main/java/com/example/immo/services/FileService.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,13 +23,9 @@ public class FileService {
     // should check if file not already exists
     // check if jpg / png
     public String save(MultipartFile file) {
-        // String dir = System.getProperty("user.dir") + "/" + filePath;
         String dir = System.getProperty("user.dir") + "/src/main/resources/static/" + filePath;
         try {
-            // file.transferTo(new File(dir + "/" + file.getOriginalFilename()));
             String filename = generateUniqueFileName("img-", ".jpg");
-            // file.transferTo(new File(dir + "/" + file.getOriginalFilename()));
-            // file.transferTo(new File(dir + "/" + filename));
             file.transferTo(new File(dir + filename));
             return filename;
         } catch (IOException e) {
@@ -37,19 +34,31 @@ public class FileService {
         }
     }
 
-    public InputStream getResource(String fileName) throws IOException {
+    /*public InputStream getResource(String fileName) throws IOException {
         // String IMAGE_FOLDER = System.getProperty("user.dir") + "/" + filePath;
         // String fullPath = IMAGE_FOLDER + "/" + fileName;
         var imgFile = new ClassPathResource("static/uploads/" + fileName);
         return imgFile.getInputStream();
         // return new FileInputStream(fullPath);
+    }*/
+
+    public Resource getImgResource(String fileName) throws IOException{
+        String IMAGE_FOLDER = System.getProperty("user.dir") + "/src/main/resources/static/" + filePath;
+        Path imagePath = Paths.get(IMAGE_FOLDER).resolve(fileName);
+        return new UrlResource(imagePath.toUri());
     }
 
+    /*
     public Resource getResource2(String fileName) throws IOException {
         String IMAGE_FOLDER = System.getProperty("user.dir") + "/" + filePath;
         Path imagePath = Paths.get(IMAGE_FOLDER).resolve(fileName);
         return new UrlResource(imagePath.toUri());
     }
+
+    public InputStream getInputStream(String filename) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:static/uploads/" + filename);
+        return resource.getInputStream();
+    }*/
 
     public static String generateUniqueFileName(String prefix, String suffix) {
         return (prefix != null ? prefix : "") + System.nanoTime() + (suffix != null ? suffix : "");
