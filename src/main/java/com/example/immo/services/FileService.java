688cdf53500bf6464159dbb8f669132dcf7fa2ab
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-03-23 12:32:53 +0100

    Message Service file type checking

--- src/main/java/com/example/immo/services/FileService.java
+++ src/main/java/com/example/immo/services/FileService.java
@@ -2,16 +2,16 @@ package com.example.immo.services;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -25,22 +25,18 @@ public class FileService {
     public String save(MultipartFile file) {
         String dir = System.getProperty("user.dir") + "/src/main/resources/static/" + filePath;
         try {
-            String filename = generateUniqueFileName("img-", ".jpg");
+            String filename = "";
+            // check if png or jpg file & generate a unique filename
+            if(Objects.equals(file.getContentType(), "image/jpeg")) filename = generateUniqueFileName("img-", ".jpg");
+            if(Objects.equals(file.getContentType(), "image/png")) filename = generateUniqueFileName("img-", ".png");
+            if(filename.isEmpty()) throw new RuntimeException("Unsupported File Type.");
             file.transferTo(new File(dir + filename));
             return filename;
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             throw new RuntimeException(e);
         }
     }
-
-    /*public InputStream getResource(String fileName) throws IOException {
-        // String IMAGE_FOLDER = System.getProperty("user.dir") + "/" + filePath;
-        // String fullPath = IMAGE_FOLDER + "/" + fileName;
-        var imgFile = new ClassPathResource("static/uploads/" + fileName);
-        return imgFile.getInputStream();
-        // return new FileInputStream(fullPath);
-    }*/
 
     public Resource getImgResource(String fileName) throws IOException{
         String IMAGE_FOLDER = System.getProperty("user.dir") + "/src/main/resources/static/" + filePath;
@@ -48,19 +44,20 @@ public class FileService {
         return new UrlResource(imagePath.toUri());
     }
 
-    /*
-    public Resource getResource2(String fileName) throws IOException {
-        String IMAGE_FOLDER = System.getProperty("user.dir") + "/" + filePath;
-        Path imagePath = Paths.get(IMAGE_FOLDER).resolve(fileName);
-        return new UrlResource(imagePath.toUri());
-    }
-
-    public InputStream getInputStream(String filename) throws IOException {
-        Resource resource = resourceLoader.getResource("classpath:static/uploads/" + filename);
-        return resource.getInputStream();
-    }*/
-
     public static String generateUniqueFileName(String prefix, String suffix) {
         return (prefix != null ? prefix : "") + System.nanoTime() + (suffix != null ? suffix : "");
+    }
+
+    public void deleteAll() {
+        String IMAGE_FOLDER = System.getProperty("user.dir") + "/src/main/resources/static/" + filePath;
+        Path imagePath = Paths.get(IMAGE_FOLDER);
+        FileSystemUtils.deleteRecursively(imagePath.toFile());
+        try {
+            // Create the directory
+            Files.createDirectories(imagePath);
+            System.out.println("Directory created successfully.");
+        } catch (IOException e) {
+            System.err.println("Failed to create directory: " + e.getMessage());
+        }
     }
 }
