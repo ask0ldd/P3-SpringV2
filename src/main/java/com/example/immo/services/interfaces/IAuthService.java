a0406d9a4acba1f65c23194b7712f6fb7104b759
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-03-23 12:18:36 +0100

    Minor Interfaces updates

--- src/main/java/com/example/immo/services/interfaces/IAuthService.java
+++ src/main/java/com/example/immo/services/interfaces/IAuthService.java
@@ -5,9 +5,9 @@ import com.example.immo.models.User;
 
 public interface IAuthService {
 
-    TokenResponseDto registerUser(String email, String username, String password);
+    String registerUser(String email, String username, String password);
 
-    TokenResponseDto loginUser(String email, String password);
+    String loginUser(String email, String password);
 
     // User getUserInfos();
 }
--- src/main/java/com/example/immo/services/interfaces/IRentalService.java
+++ src/main/java/com/example/immo/services/interfaces/IRentalService.java
@@ -5,14 +5,7 @@ import com.example.immo.models.Rental;
 
 public interface IRentalService {
     Rental getRental(final Integer id);
-
-    RentalResponseDto getReturnableRental(final Integer id);
-
     Iterable<Rental> getRentals();
-
-    Iterable<RentalResponseDto> getReturnableRentals();
-
     Rental saveRental(Rental rental);
-
     void deleteRental(final Integer id);
 }
