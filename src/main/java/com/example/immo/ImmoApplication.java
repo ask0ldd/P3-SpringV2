372ec957885f74f995d5eea1d31e101f070ffa29
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-03-24 01:30:49 +0100

    main class db init simplified

--- src/main/java/com/example/immo/ImmoApplication.java
+++ src/main/java/com/example/immo/ImmoApplication.java
@@ -2,23 +2,20 @@ package com.example.immo;
 
 import com.example.immo.models.Role;
 import com.example.immo.repositories.RoleRepository;
-import com.example.immo.services.FileService;
+import com.example.immo.services.FileSystemService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 @SpringBootApplication
 public class ImmoApplication implements CommandLineRunner { // !!! commandlinerunner added soutenance
 
 	private final RoleRepository roleRepository;
-	private final FileService fileService;
+	private final FileSystemService fileSystemService;
 
-	public ImmoApplication(RoleRepository roleRepository, FileService fileService) {
+	public ImmoApplication(RoleRepository roleRepository, FileSystemService fileSystemService) {
 		this.roleRepository = roleRepository;
-		this.fileService=fileService;
+		this.fileSystemService = fileSystemService;
 	}
 
 	public static void main(String[] args) {
@@ -28,21 +25,12 @@ public class ImmoApplication implements CommandLineRunner { // !!! commandlineru
 	@Override
 	public void run(String... args) throws Exception {
 		// emptying the folder containing all the uploaded pictures
-		fileService.deleteAll();
+		fileSystemService.deleteAllFiles();
 		// init role table
-		if (roleRepository.findByAuthority("ADMIN").isPresent()) return; // if empty
-		roleRepository.save(new Role("ADMIN"));
-		roleRepository.save(new Role("USER"));
-		/*roleRepository.findByAuthority("ADMIN").orElseGet(() -> {
-			roleRepository.save(new Role("ADMIN"));
-			return roleRepository.save(new Role("USER"));
-		}); // if em*/
-		/*roleRepository.findByAuthority("ADMIN").ifPresentOrElse(a -> {}, () -> {
+		roleRepository.findByAuthority("ADMIN").ifPresentOrElse(a -> {}, () -> {
 			roleRepository.save(new Role("ADMIN"));
 			roleRepository.save(new Role("USER"));
-		});*/
-
-		// BiFunction
+		});
 	}
 
 }
