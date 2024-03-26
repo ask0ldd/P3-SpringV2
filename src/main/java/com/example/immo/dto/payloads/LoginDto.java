c21d667434fc8eeb0b83822f73ad97dae91320dc
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-03-26 13:30:01 +0100

    moving login & registration dto to payloads

--- src/main/java/com/example/immo/dto/payloads/LoginDto.java
+++ src/main/java/com/example/immo/dto/payloads/LoginDto.java
@@ -1,12 +1,17 @@
 package com.example.immo.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class LoginDto {
+    @Email(message = "Invalid email format")
     private String email;
+
+    @Size(min=3, max=32, message="The password must be between {min} and {max} characters long")
     private String password;
 
     public LoginDto(String email, String password) {
--- src/main/java/com/example/immo/dto/payloads/RegistrationDto.java
+++ src/main/java/com/example/immo/dto/payloads/RegistrationDto.java
@@ -1,13 +1,23 @@
 package com.example.immo.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class RegistrationDto {
+
+    @NotEmpty
+    @Size(min=3, max=128, message="The username must be between {min} and {max} characters long")
     private String username;
+
+    @Email(message = "Invalid email format")
     private String email;
+
+    @Size(min=3, max=32, message="The password must be between {min} and {max} characters long")
     private String password;
 
     public RegistrationDto(String email, String name, String password) {
