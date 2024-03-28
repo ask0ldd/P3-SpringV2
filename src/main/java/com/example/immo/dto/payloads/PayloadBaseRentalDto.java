df04138790beae1616e46914d626918cca0c5aed
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-03-28 13:16:58 +0100

    *edit* PayloadBaseRentalDto validation constraints added

--- src/main/java/com/example/immo/dto/payloads/PayloadBaseRentalDto.java
+++ src/main/java/com/example/immo/dto/payloads/PayloadBaseRentalDto.java
@@ -2,6 +2,8 @@ package com.example.immo.dto.payloads;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -28,9 +30,13 @@ public class PayloadBaseRentalDto {
 
     @Schema(description = "The surface of the rental")
     @JsonProperty("surface")
+    @Min(value = 0, message = "The surface must be greater than or equal to 0")
+    @Max(value = 1000, message = "The surface must be less than or equal to 1000")
     private Integer surface;
 
     @Schema(description = "The price of the rental")
     @JsonProperty("price")
+    @Min(value = 0, message = "The price must be greater than or equal to 0")
+    @Max(value = 10000, message = "The price must be less than or equal to 10000")
     private Integer price;
 }
