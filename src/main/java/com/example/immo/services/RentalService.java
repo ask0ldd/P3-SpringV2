b6e4fe725c8cbae1238b968dbd672b22fd1e660d
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-03-28 13:17:48 +0100

    *add* rental service impl

---
+++ src/main/java/com/example/immo/services/RentalService.java
@@ -0,0 +1,42 @@
+package com.example.immo.services;
+
+import java.util.Optional;
+
+import com.example.immo.exceptions.RentalNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.immo.models.Rental;
+import com.example.immo.repositories.RentalRepository;
+import com.example.immo.services.interfaces.IRentalService;
+
+import lombok.Data;
+
+@Service
+public class RentalService implements IRentalService {
+
+    private final RentalRepository rentalRepository;
+
+    public RentalService(RentalRepository rentalRepository) {
+        this.rentalRepository = rentalRepository;
+    }
+
+    public Rental getRental(final Integer id) {
+        return rentalRepository.findById(id)
+                .orElseThrow(() -> new RentalNotFoundException("Target rental cannot be found."));
+    }
+
+    public Iterable<Rental> getRentals() {
+        return rentalRepository.findAll();
+    }
+
+    public Rental saveRental(Rental rental) {
+        return Optional.of(rentalRepository.save(rental))
+                .orElseThrow(() -> new RuntimeException("Failed to save the rental."));
+    }
+
+    public void deleteRental(final Integer id) {
+        Rental rental = rentalRepository.findById(id)
+                .orElseThrow(() -> new RentalNotFoundException("Target rental cannot be found."));
+        rentalRepository.delete(rental);
+    }
+}
