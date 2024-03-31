2754b8f14910870c17901d0d56d4ee08e6d4f675
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-03-31 03:56:27 +0200

    *fix* swagger 200 response had the wrong dto associated

--- src/main/java/com/example/immo/controllers/UserController.java
+++ src/main/java/com/example/immo/controllers/UserController.java
@@ -30,7 +30,7 @@ public class UserController {
     @GetMapping("/user/{id}")
     @Operation(summary = "Retrieve a target user", description = "Endpoint to retrieved a target user.", tags = {"User"})
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "User retrieved.", content = @Content(schema = @Schema(implementation = TokenResponseDto.class))),
+            @ApiResponse(responseCode = "200", description = "User retrieved.", content = @Content(schema = @Schema(implementation = UserResponseDto.class))),
             @ApiResponse(responseCode = "404", description = "User not found."),
     })
     public ResponseEntity<?> getUser(@PathVariable("id") final Integer id) {
